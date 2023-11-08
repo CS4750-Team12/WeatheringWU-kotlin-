601687e6e4ac9e214171ec38a68f6289557d5eeb
BenLL <zihaoluo153@gmail.com>
2023-11-07 17:38:32 -0800

    search bar + skeleton code for api

--- app/src/main/java/com/example/weatheringwu/MainActivity.kt
+++ app/src/main/java/com/example/weatheringwu/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.weatheringwu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editTextCitySearch: EditText
+    private lateinit var buttonSearch: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextCitySearch = findViewById(R.id.editTextCitySearch)
+        buttonSearch = findViewById(R.id.buttonSearch)
+
+        buttonSearch.setOnClickListener{
+            val city = editTextCitySearch.text.toString()
+            if(city.isNotEmpty()){
+                fetchWeatherForCity(city)
+            }else{
+                Toast.makeText(this, "Please enter the name of a city", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun fetchWeatherForCity(city: String){
+        //todo
     }
 }
+
+//Geocoding API for longitude and latitude: https://openweathermap.org/api/geocoding-api
+//Geocoding API for deatiled weather information: https://openweathermap.org/current
